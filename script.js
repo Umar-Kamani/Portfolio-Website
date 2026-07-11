bfcfcdab0ec57599c3c6663d0b34eff8291446a6
Umar Kamani <umar.kamani@gmail.com>
2026-07-11 22:56:05 +0400

    added header class switch

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-document.addEventListener("DOMContentLoaded", () => {
+const navbar = document.querySelector('.navbar');
 
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        navbar.classList.add('is-scrolled');
+    } else {
+        navbar.classList.remove('is-scrolled');
+    }
 });
